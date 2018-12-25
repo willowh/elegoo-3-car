dcb3877391e624f70ea24ad03b00262c9870401b
Jim Pugh <jimpugh@JimsImac.local>
2018-12-25 16:14:43 -0500

    all reference code commit

---
+++ referenceCode/02-bluetooth_car.ino
@@ -0,0 +1,81 @@
+//www.elegoo.com
+
+#define ENB 5
+#define IN1 7
+#define IN2 8
+#define IN3 9
+#define IN4 11
+#define ENA 6
+
+unsigned char carSpeed = 200;
+char getstr;
+
+void forward(){
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  Serial.println("Forward");
+}
+
+void back(){
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  Serial.println("Back");
+}
+
+void left(){
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  Serial.println("Left");
+}
+
+void right(){
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  Serial.println("Right");
+}
+
+void stop(){
+  digitalWrite(ENA, LOW);
+  digitalWrite(ENB, LOW);
+  Serial.println("Stop!");
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
+  pinMode(ENA,OUTPUT);
+  pinMode(ENB,OUTPUT);
+  stop();
+}
+
+void loop() {
+  getstr = Serial.read();
+  switch(getstr){
+    case 'f': forward();  break;
+    case 'b': back();     break;
+    case 'l': left();     break;
+    case 'r': right();    break;
+    case 's': stop();     break;
+    default:              break;
+  }
+}
